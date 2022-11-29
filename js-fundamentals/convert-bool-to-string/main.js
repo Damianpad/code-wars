6ae830bea22e19f77c31005fb86f814b962cd9e3
Damian Padilla <damipad4@gmail.com>
2022-11-28 23:52:41 -0600

    completed convert to string

---
+++ js-fundamentals/convert-bool-to-string/main.js
@@ -0,0 +1,9 @@
+// Implement a function which convert the given boolean value into its string representation.
+
+//Note: Only valid inputs will be given.
+
+function convertToString(x){
+    return x.toString()
+}
+
+console.log(convertToString(true));
