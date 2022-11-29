f240794da9c015712197834d095a36903d33fcd3
Damian Padilla <damipad4@gmail.com>
2022-11-29 00:01:55 -0600

    completed grasshopper gradebook

---
+++ js-fundamentals/grasshopper-gradebook/main.js
@@ -0,0 +1,38 @@
+/*
+Grade book
+Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
+
+Numerical Score	Letter Grade
+90 <= score <= 100	'A'
+80 <= score < 90	'B'
+70 <= score < 80	'C'
+60 <= score < 70	'D'
+0 <= score < 60	'F'
+Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.
+*/
+
+function checkGrade(s1,s2,s3) {
+    let grade = (s1 + s2 +s3) / 3
+    if (grade <= 100 && grade >=90){
+        console.log('A');
+    } else if (grade <= 89 && grade >= 80) {
+        console.log('B');
+    } else if (grade <= 79 && grade >= 70) {
+        console.log("C");
+    } else {
+        console.log('D');
+    }
+}
+
+console.log(checkGrade(60, 80, 95));
+
+// Rated as best practice 
+
+function getGrade (s1, s2, s3) {
+    avg = (s1+s2+s3)/3;
+    if (avg < 60)  return "F";
+      else if (avg < 70) return "D";
+      else if (avg < 80) return "C";
+      else if (avg < 90) return "B";
+      else return "A";
+  }
