92e50a4efbca79f06da1fe13f13c5488fa420256
Damian Padilla <damipad4@gmail.com>
2022-11-28 23:46:53 -0600

    completed reduce grow

--- js-array-methods/main.js
+++ js-array-methods/main.js
@@ -2,11 +2,11 @@
 //create a program that returns true if the sum of the squares of each element in a is 
 //strictly greater than the sum of the cubes of each element in b.
 
-function compareSquareAndCubed(a,b) {
+function compareSquareAndCubed(a,b){
     return a.reduce((acc, c) => acc + c**2, 0) > b.reduce((acc, c) => acc + c**3, 0)
 }
 
-let a = [5,2,2]
+let a = [2,2,2]
 let b = [2,2,2]
 
 console.log(compareSquareAndCubed(a,b));
