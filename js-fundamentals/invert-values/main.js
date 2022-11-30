65df914c14ae51cebad0aa4e7428120247af98ae
Damian Padilla <damipad4@gmail.com>
2022-11-30 15:12:05 -0600

    added best practice solution

---
+++ js-fundamentals/invert-values/main.js
@@ -0,0 +1,15 @@
+function invert(arr){
+    let invertedArr = []
+    for (let i = 0; i < arr.length; i++){
+        invertedArr.push(arr[i] * (-1))
+    }
+    return invertedArr
+}
+
+let array = [1,-2,3,4,-4,-5,-6]
+
+console.log(invert(array));
+
+
+// Best practice Solution
+const invert = array => array.map(num => -num);
