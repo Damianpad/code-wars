f2b6eeaf4bdcf2853bf5106f94c981a52cfbf524
Damian Padilla <damipad4@gmail.com>
2022-12-04 23:44:43 -0600

    completed check for factor problem

---
+++ js-fundamentals/check-for-factor/main.js
@@ -0,0 +1,4 @@
+const checkForFactor = (base,factor) => { return base % factor == 0 ? true : false }
+
+// Best Practice and Readable
+// const checkForFactor = (base, factor) => base % factor === 0;
