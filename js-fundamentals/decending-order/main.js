83bc4a2e08f75cb3662aab43c30abdf3a67967ae
Damian Padilla <damipad4@gmail.com>
2022-12-06 17:38:33 -0600

    completed understanding of descending order

--- js-fundamentals/decending-order/main.js
+++ js-fundamentals/decending-order/main.js
@@ -16,3 +16,32 @@ function descendingOrder(n){
 }
 
 console.log(descendingOrder(123564541));
+
+// Understand:
+
+//Create a function that takes in a number
+// Convert that number into a string
+// Then split that string into individual elements of an array
+// sort that that array into ascending order
+// reserve that order so that the elements are descending order
+// join the elements to a new array
+// convert result back into an array
+
+// function descendingOrder(n){
+//     return parseInt(String(n).split('').sort().reverse().join(''))
+// }
+
+// Code Weenie Answer
+
+// const descendingOrder = n => parseInt(String(n).split('').sort().reverse().join(''))
+
+console.log(descendingOrder(54712541251));
+
+
+
+
+// If you want to arrange a number into ascending order you can just omit the .reverse() method
+function ascendingOrder(n){
+    return parseInt(String(n).split('').sort().join(''))
+}
+console.log(ascendingOrder(54712541251));
