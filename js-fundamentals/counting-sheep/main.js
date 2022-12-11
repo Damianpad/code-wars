c676658e849bacda647aa57308441c2669ab5da3
Damian Padilla <damipad4@gmail.com>
2022-12-11 02:30:39 -0600

    completed counting sheep. included best practice solution

--- js-fundamentals/counting-sheep/main.js
+++ js-fundamentals/counting-sheep/main.js
@@ -21,15 +21,38 @@ Hint: Don't forget to check for bad values like null/undefined
 // Return: The function will return the number of sheep present
 
 // Example:
-function sheepPresent(arr){
+// function sheepPresent(arr){
 
-}
+// }
 
-sheepPresent([true, false]) // 1 sheep present
-sheepPresent([false, false, false]) // 0 sheep present
-sheepPresent([true, true, false, false]) // 2 sheep present
+// sheepPresent([true, false]) // 1 sheep present
+// sheepPresent([false, false, false]) // 0 sheep present
+// sheepPresent([true, true, false, false]) // 2 sheep present
 
 // Pseudo Code
 // Write a function that takes in an arry of sheep
 // Check how many items in the array are true
 // Return the number of true (present)
+
+function sheepPresent(arr){
+    let count = 0
+    for (i=0; i < arr.length; i++){
+        if (arr[i] == true){
+            count += 1   
+        }
+    }
+    return count
+}
+
+arr1 = [true, true, true]
+arr2 = [true, false]
+arr3 = [false]
+
+sheepPresent(arr1)
+sheepPresent(arr2)
+sheepPresent(arr3)
+
+// Rated Best Practice: 
+function countSheeps(arrayOfSheeps) {
+  return arrayOfSheeps.filter(Boolean).length;
+}
