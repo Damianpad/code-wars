c244bd4f339fbe70837309b97c6717b3fe6bbf74
Damian Padilla <damipad4@gmail.com>
2022-12-24 19:50:34 -0600

    init string ends with solution

---
+++ js-fundamentals/strings-ends-with/main.js
@@ -0,0 +1,28 @@
+/*
+Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+Examples:
+
+solution('abc', 'bc') // returns true
+solution('abc', 'd') // returns false
+
+PREP
+Parameter:
+A String
+if ends with the 2nd then true
+
+Return:
+Boolean 
+
+Example:
+function solution(str, ending){
+
+}
+
+Pseduo Code:
+Write a function that takes in a string
+Check if it ends with str
+Return True if it does
+False if not
+
+*/
