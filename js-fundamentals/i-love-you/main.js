9ec49c9157870df7ab9765d38d5049b71261ebbb
Damian Padilla <damipad4@gmail.com>
2022-12-28 21:12:57 -0600

    completed I love you

--- js-fundamentals/i-love-you/main.js
+++ js-fundamentals/i-love-you/main.js
@@ -12,4 +12,34 @@ If there are more than 6 petals, you start over with "I love you" for 7 petals, 
 When the last petal was torn there were cries of excitement, dreams, surging thoughts and emotions.
 
 Your goal in this kata is to determine which phrase the girls would say at the last petal for a flower of a given number of petals. The number of petals is always greater than 0.
+
+PREP
+Parameter:
+Number Int
+
+Return:
+A string. One of the strings provided
+
+Example:
+function howMuchILoveYou(nbPetals) {
+
+}
+
+howMuchILoveYou(2){
+    return "a little"
+}
+
+Pseudo:
+
+Store phrases in an array 
+Run function and whatever num is passed in will be the result -1
+
 */
+
+function howMuchILoveYou(num){
+    const phrase = ["I love you", "a little", "a lot", "passionately", "madly", "not at all"]
+
+    return phrase[(num - 1) % phrase.length]
+}
+
+console.log(howMuchILoveYou(1));
